91359cae1cd10b20b6134cfaef8f8ee5feb5d51a
board817 <14579898+board817@users.noreply.github.com>
2023-07-26 16:12:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,22 @@
+// create web server
+
+// import modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/comments');
+
+// create express app
+var app = express();
+
+// use body parser middleware to parse json
+app.use(bodyParser.json());
+
+// use cors middleware
+app.use(cors());
+
+// create mongoose schema
+var Comment = mongoose.model('Comment', {
